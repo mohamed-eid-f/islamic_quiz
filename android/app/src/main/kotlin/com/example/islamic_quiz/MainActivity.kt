359a4da11463ecf9ae038db0b414899e7bef8d5c
Mohamed Eid <mohamed.eid.flutter@gmail.com>
2024-11-02 17:14:21 +0200

    downgrading sdk

--- android/app/src/main/kotlin/com/example/islamic_quiz/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/islamic_quiz/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.islamic_quiz
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,18 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:hive_flutter/hive_flutter.dart";
 import "package:islamic_quiz/app_localizations.dart";
 import "package:islamic_quiz/features/quiz/data/model/question_model.dart";
-import "package:islamic_quiz/features/quiz/data/repository/database_manager.dart";
-import "package:islamic_quiz/features/quiz/data/repository/hive_questions_repo.dart";
-import "package:islamic_quiz/features/quiz/presentation/view_model/question_repository_provider.dart";
+// import "package:islamic_quiz/features/quiz/presentation/views/test_view.dart";
 import "package:islamic_quiz/features/quiz/presentation/views/quiz_view.dart";
-import "package:islamic_quiz/features/quiz/presentation/views/test_view.dart";
-
+import "package:flutter_screenutil/flutter_screenutil.dart";
 import "core/theme/app_theme.dart";
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox<QuestionModel>("questions");
+  await ScreenUtil.ensureScreenSize();
+
+  // 1440 X 2560
+  // flutter: 3.16.6
 
   runApp(
     const ProviderScope(
